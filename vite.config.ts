cfd0a142c3744b1adabb32f4c56ae77d8ce1c19a
tatyana.lebedeva <tslebedeva3103@gmail.com>
2024-11-28 22:20:58 +0100

    add gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from 'node:url';
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import vueJsx from '@vitejs/plugin-vue-jsx';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  base: '/octo-potato/',
+  plugins: [
+    vue(),
+    vueJsx(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
 })
